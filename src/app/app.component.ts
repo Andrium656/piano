008a6a93016dbcdf9640f54c8ff145645e30ea06
Andres Florez <andrium656@gmail.com>
2024-04-09 08:52:18 -0500

    Piano en Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Piano';
+
+  aplicarSonido(numero: number): void {
+    const audio = new Audio();
+    audio.src = `../assets/sonidos/note${numero}.wav`;
+    audio.load(); //Cargar el audio
+    audio.play(); //Reproducir el audio
+  }
 }
